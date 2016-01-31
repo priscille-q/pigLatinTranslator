4435a8f08d06d4704d7689d9a3ebf8bb72288025
priscille <arka.darell@gmail.com>
2016-01-31 14:36:08 +0000

    Add parser and delete LatinAlphabetTest

---
+++ src/Model/Parser.php
@@ -0,0 +1,26 @@
+<?php
+namespace priscille_q\pigLatinTranslator\Model;
+
+class Parser implements ParserInterface
+{
+	const VOWEL = 'aeiou';
+
+	public function isBeginByVowel($word)
+	{
+		if (!isset($word) || empty($word))
+		{
+			return false;
+		}
+		return preg_match('/^['. self::VOWEL .']{1}/i', $word);
+
+	}
+
+	public function getPositionFirstVowel($word)
+	{
+		if (!isset($word) || empty($word) || !is_string($word))
+		{
+			return false;
+		}
+		return strcspn(strtolower($word), self::VOWEL);
+	}
+}
---
+++ tests/ParserTest.php
@@ -0,0 +1,129 @@
+<?php
+use priscille_q\pigLatinTranslator\Autoloader;
+use priscille_q\pigLatinTranslator\Model\Parser;
+require_once __DIR__ .'/../src/Model/Autoloader.php';
+
+Autoloader::autoloadregister();
+
+class ParserTest extends PHPUnit_Framework_TestCase
+{
+	public function testConstructor()
+	{
+		new Parser();
+	}
+
+	public function testNumberAsParameterForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel(42);
+		$this->assertEquals($result, false);
+	}
+
+	public function testSpecialCharacterAsParameterForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel('^&*');
+		$this->assertEquals($result, false);
+	}
+
+	public function testSpaceAsParameterForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel('  ');
+		$this->assertEquals($result, false);
+	}
+
+	public function testEmptyStringAsParameterForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel('');
+		$this->assertEquals($result, false);
+	}
+
+	public function testNullAsParameterForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel(null);
+		$this->assertEquals($result, false);
+	}
+
+	public function testVowelUpperCaseForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel('Used');
+		$this->assertEquals($result, true);
+	}
+
+	public function testVowelLowerCaseForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel('used');
+		$this->assertEquals($result, true);
+	}
+
+	public function testNoVowelUpperCaseForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel('Beast');
+		$this->assertEquals($result, false);
+	}
+
+	public function testNoVowelLowerCaseForIsBeginByVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->isBeginByVowel('beast');
+		$this->assertEquals($result, false);
+	}
+
+
+	public function testNumberAsParameterForGetPositionFirstVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->getPositionFirstVowel(42);
+		$this->assertEquals($result, false);
+	}
+
+	public function testSpecialCharacterAsParameterForGetPositionFirstVowel()
+	{
+		$parser = new Parser();
+		$word = '^&*';
+		$result = $parser->getPositionFirstVowel($word);
+		$this->assertEquals($result, strlen($word));
+	}
+
+	public function testSpaceAsParameterForGetPositionFirstVowel()
+	{
+		$parser = new Parser();
+		$word = ' ';
+		$result = $parser->getPositionFirstVowel($word);
+		$this->assertEquals($result, strlen($word));
+	}
+
+	public function testEmptyStringAsParameterForGetPositionFirstVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->getPositionFirstVowel('');
+		$this->assertEquals($result, false);
+	}
+
+	public function testNullAsParameterForGetPositionFirstVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->getPositionFirstVowel(null);
+		$this->assertEquals($result, false);
+	}
+
+	public function testVowelUpperCaseForGetPositionFirstVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->getPositionFirstVowel('BEAST');
+		$this->assertEquals($result, 1);
+	}
+
+	public function testVowelLowerCaseForGetPositionFirstVowel()
+	{
+		$parser = new Parser();
+		$result = $parser->getPositionFirstVowel('beast');
+		$this->assertEquals($result, 1);
+	}
+}
